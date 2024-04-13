8fe7af55013620bd474683b8ffc0f25c51c86727
evador <evador@ucenfotec.ac.cr>
2024-04-13 12:31:06 -0600

    Last-changes

--- Paso 4/JS/Language-Change.js
+++ Paso 4/JS/Language-Change.js
@@ -7,6 +7,7 @@ async function loadLanguage(language) {
         const response = await fetch('../Assets/Languages/' + language + '.json');
         const data = await response.json();
 
+
         document.getElementById('about').textContent = data.about;
         document.getElementById('tools').textContent = data.tools;
         document.getElementById('works').textContent = data.works;
@@ -28,6 +29,27 @@ async function loadLanguage(language) {
         document.getElementById('projectDesOne').textContent = data.projectDesc;
         document.getElementById('projectTitleTwo').textContent = data.projectTitleTwo;
         document.getElementById('projectDesTwo').textContent = data.projectDescTwo;
+        document.getElementById('start').textContent = data.home;
+        document.getElementById('tools-desc-box').textContent = data.toolsDescBox;
+        document.getElementById('projects-desc-box').textContent = data.projectsDescBox;
+        document.getElementById('contact-desc-box').textContent = data.contactDescBox;
+        document.getElementById('toolD1').textContent = data.toolD1;
+        document.getElementById('toolD2').textContent = data.toolD2;
+        document.getElementById('toolD3').textContent = data.toolD3;
+        document.getElementById('toolD4').textContent = data.toolD4;
+        document.getElementById('toolF1').textContent = data.toolF1;
+        document.getElementById('toolF2').textContent = data.toolF2;
+        document.getElementById('toolF3').textContent = data.toolF3;
+        document.getElementById('toolF4').textContent = data.toolF4;
+        document.getElementById('toolB1').textContent = data.toolB1;
+        document.getElementById('toolB2').textContent = data.toolB2;
+        document.getElementById('toolB3').textContent = data.toolB3;
+        document.getElementById('toolB4').textContent = data.toolB4;
+        document.getElementById('toolBC1').textContent = data.toolBC1;
+        document.getElementById('toolBC2').textContent = data.toolBC2;
+        document.getElementById('toolBC3').textContent = data.toolBC3;
+        document.getElementById('toolBC4').textContent = data.toolBC4;
+        
 
         carrousel = data.welcomeMessages;
         sweetText = data.sweetAlertText;
